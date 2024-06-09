444c3f1964f2ac02126627f20b10f1e35bd386f8
Azam <146977640+aliaxam153@users.noreply.github.com>
2024-06-09 10:28:09 -0700

    Add files via upload

---
+++ Thirdparty/Sophus/build/test/core/CTestTestfile.cmake
@@ -0,0 +1,28 @@
+# CMake generated Testfile for 
+# Source directory: /home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core
+# Build directory: /home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/build/test/core
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_common "test_common")
+set_tests_properties(test_common PROPERTIES  _BACKTRACE_TRIPLES "/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;12;ADD_TEST;/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;0;")
+add_test(test_so2 "test_so2")
+set_tests_properties(test_so2 PROPERTIES  _BACKTRACE_TRIPLES "/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;12;ADD_TEST;/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;0;")
+add_test(test_se2 "test_se2")
+set_tests_properties(test_se2 PROPERTIES  _BACKTRACE_TRIPLES "/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;12;ADD_TEST;/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;0;")
+add_test(test_rxso2 "test_rxso2")
+set_tests_properties(test_rxso2 PROPERTIES  _BACKTRACE_TRIPLES "/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;12;ADD_TEST;/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;0;")
+add_test(test_sim2 "test_sim2")
+set_tests_properties(test_sim2 PROPERTIES  _BACKTRACE_TRIPLES "/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;12;ADD_TEST;/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;0;")
+add_test(test_so3 "test_so3")
+set_tests_properties(test_so3 PROPERTIES  _BACKTRACE_TRIPLES "/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;12;ADD_TEST;/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;0;")
+add_test(test_se3 "test_se3")
+set_tests_properties(test_se3 PROPERTIES  _BACKTRACE_TRIPLES "/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;12;ADD_TEST;/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;0;")
+add_test(test_rxso3 "test_rxso3")
+set_tests_properties(test_rxso3 PROPERTIES  _BACKTRACE_TRIPLES "/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;12;ADD_TEST;/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;0;")
+add_test(test_sim3 "test_sim3")
+set_tests_properties(test_sim3 PROPERTIES  _BACKTRACE_TRIPLES "/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;12;ADD_TEST;/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;0;")
+add_test(test_velocities "test_velocities")
+set_tests_properties(test_velocities PROPERTIES  _BACKTRACE_TRIPLES "/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;12;ADD_TEST;/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;0;")
+add_test(test_geometry "test_geometry")
+set_tests_properties(test_geometry PROPERTIES  _BACKTRACE_TRIPLES "/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;12;ADD_TEST;/home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/core/CMakeLists.txt;0;")
