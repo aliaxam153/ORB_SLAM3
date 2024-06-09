82269f53fd4e04ebac40fa712573be8fb8eab220
Azam <146977640+aliaxam153@users.noreply.github.com>
2024-06-09 10:24:07 -0700

    Add files via upload

---
+++ Thirdparty/Sophus/build/test/ceres/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/test/ceres
+# Build directory: /home/aazam/dev/ORB_SLAM3/Thirdparty/Sophus/build/test/ceres
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
