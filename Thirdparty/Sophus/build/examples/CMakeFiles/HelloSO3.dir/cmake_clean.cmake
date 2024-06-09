b2c751ad341db913af9a6e05b751b966f00a65fa
Azam <146977640+aliaxam153@users.noreply.github.com>
2024-06-09 10:21:08 -0700

    Add files via upload

---
+++ Thirdparty/Sophus/build/examples/CMakeFiles/HelloSO3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HelloSO3.dir/HelloSO3.cpp.o"
+  "HelloSO3"
+  "HelloSO3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HelloSO3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
